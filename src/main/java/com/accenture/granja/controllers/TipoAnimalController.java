9cd0b12b2670e16c317fbabb91c8a5816a22a759
Carracedo <marinacarracedo14@gmail.com>
2024-02-20 11:26:30 -0300

    CrossOrigin en Controllers

--- src/main/java/com/accenture/granja/controllers/TipoAnimalController.java
+++ src/main/java/com/accenture/granja/controllers/TipoAnimalController.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.accenture.granja.model.TiposAnimales;
 import com.accenture.granja.services.TipoAnimalService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") 
 public class TipoAnimalController {
 
 	// Aca se instancia al Servicio donde esta la logica central
--- src/main/java/com/accenture/granja/controllers/VentaController.java
+++ src/main/java/com/accenture/granja/controllers/VentaController.java
@@ -3,6 +3,7 @@ package com.accenture.granja.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.accenture.granja.model.Venta;
 import com.accenture.granja.services.VentaService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200") 
 public class VentaController {
 	@Autowired
 	private VentaService ventaService;
