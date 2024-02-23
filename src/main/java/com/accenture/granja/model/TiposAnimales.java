b50f1ee9c3d29e0f6a49cbb4653e516a83561e57
Carracedo <marinacarracedo14@gmail.com>
2024-02-23 09:56:47 -0300

    Arreglo get granja ventas compras

--- src/main/java/com/accenture/granja/model/TiposAnimales.java
+++ src/main/java/com/accenture/granja/model/TiposAnimales.java
@@ -43,7 +43,7 @@ public class TiposAnimales {
 	private String imagen;
 	
 	@OneToMany(mappedBy = "tiposAnimales", cascade = CascadeType.ALL, orphanRemoval = true)
-	@JsonIgnore
+	//@JsonIgnore
 	public List<Animal> animales;
 	
 	@NonNull
